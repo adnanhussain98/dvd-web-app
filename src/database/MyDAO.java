01fbcbe635833e535d1e885593b20d712fb6d6f4
Adnan Hussain <adnanhussaiin98@gmail.com>
2019-12-07 16:22:09 +0000

    final

--- src/database/MyDAO.java
+++ src/database/MyDAO.java
@@ -11,10 +11,17 @@ import java.util.ArrayList;
 import models.DVD;
 import models.User;
 
+/**
+ * This class is the DA0 (Data Acessor Object). This class establishes a
+ * connection to the database and it contains methods that will communicate with
+ * the database
+ **/
+
 public class MyDAO {
 
 	private Connection getConnection() {
 
+		// creating connection object
 		Connection conn = null;
 
 		try {
@@ -32,112 +39,194 @@ public class MyDAO {
 		return conn;
 	}
 
+	// instead of returning void we want to return all dvds as objects - because
+	// java will understand it and we can work with these objects
+	// Returning an array list
 	public ArrayList<DVD> getDVDs() throws SQLException {
 
+		// initialising array list
 		ArrayList<DVD> allDvds = new ArrayList<>();
 
+		// creating connection object inside the method
 		Connection connection = getConnection();
+		// statement object created which will hold the sql string that will be
+		// executed.
 		Statement statement = connection.createStatement();
-
+		// adding the question marks to protect from SQL injection. question marks
+		// represent parameters that are passed in to the sql
+		// gets all dvd's from the dvd table (collection) and stores it in sql string
 		String sql = "SELECT * FROM collection;";
-
+		// prints the sql statement that has been executed
 		System.out.println(sql);
-
+		// result set catches the sql query result and then executes it.
 		ResultSet rs = statement.executeQuery(sql);
-
+		// while function iterates over each row until there's no more rows
 		while (rs.next()) {
+			// uses getters from DVD class and stores them in rs object.
 			int id = rs.getInt("ID");
 			String title = rs.getString("Title");
 			String genre = rs.getString("Genre");
 			int year = rs.getInt("Year");
-
+			// prints each row it has iterated over. Empty strings in sysout hold's the DVD
+			// information
 			System.out.println(id + " " + title + " " + genre + " " + year + " ");
 
 			// create new DVD object
 			DVD dvd = new DVD(id, title, genre, year);
-
+			// adds the arguments from the dvd object to the allDvds array list
 			allDvds.add(dvd);
 
 		}
+		// Closing the connection as if you do not close it it will consume your
+		// resource
+		// continuously and will have overall impact on your application and it
+		// may also not be added or returned to the pool.
 		connection.close();
+		// Returning the all the dvds from the arraylist that holds the dvd objects
 		return allDvds;
 	}
 
 	public DVD getSingleDvd(int dvdId) throws SQLException {
-
+		// creating connection object inside the method
 		Connection connection = getConnection();
-		Statement statement = connection.createStatement();
+		// declaring object of DVD class. It has not been instantiated therefore it is
+		// null.
+		// must have a value before its value is used
 		DVD dvd = null;
+		// adding the question marks to protect from SQL injection. question marks
+		// represent parameters that are passed in to the sql
+		// gets single dvd's from the dvd table (collection) where the ID = "" and
+		// stores it in sql string
+		String sql = "SELECT * FROM collection WHERE id = ?";
+		// PreparedStatement prevents SQL injection and executes faster
+		// statement object created and sql string is passed as an argument.
+		PreparedStatement statement = connection.prepareStatement(sql);
 
-		String sql = "SELECT * FROM collection WHERE id = " + dvdId + ";";
-
+		// the numbers represent the parameters in the query.
+		// 1 represents the first parameter in the query.
+		statement.setInt(1, dvdId);
+		// prints the sql statement that has been executed.
 		System.out.println(sql);
-
-		ResultSet rs = statement.executeQuery(sql);
-
+		// result set catches the sql query result and then executes it.
+		// sql does not need to be passed as an argument as the prepared statement holds
+		// it.
+		ResultSet rs = statement.executeQuery();
+		// while function iterates over each row until there's no more rows
 		while (rs.next()) {
+			// uses getters from DVD class and stores them in rs object.
 			int id = rs.getInt("ID");
 			String title = rs.getString("Title");
 			String genre = rs.getString("Genre");
 			int year = rs.getInt("Year");
-
+			// prints each row it has iterated over. Empty strings in sysout hold's the DVD
+			// information
 			System.out.println(id + " " + title + " " + genre + " " + year + " ");
 
-			// create new DVD object
+			// create new DVD object and stores the variables
 			dvd = new DVD(id, title, genre, year);
 
-			// connection.close();
-
 		}
+		// Closing the connection as if you do not close it it will consume your
+		// resource
+		// continuously and will have overall impact on your application and it
+		// may also not be added or returned to the pool.
 		connection.close();
 		return dvd;
 	}
 
 	public void insertDVD(DVD dvd) throws SQLException {
-
+		// creating connection object inside the method
 		Connection connection = getConnection();
-		Statement statement = connection.createStatement();
+		// adding the question marks to protect from SQL injection. question marks
+		// represent parameters that are passed in to the sql
+		// inserts values into title, genre, year field and stores it in sql string
+		String sql = "INSERT INTO collection (Title, Genre, Year)" + " VALUES (?, ?, ?)";
+		// PreparedStatement prevents SQL injection and executes faster
+		// statement object created and sql string is passed as an argument.
+		PreparedStatement statement = connection.prepareStatement(sql);
 
-		String sql = "INSERT INTO collection ( Title, Genre, Year )" + " VALUES ('" + dvd.getTitle() + "', '"
-				+ dvd.getGenre() + "', " + dvd.getYear() + ");";
+		// the numbers represent the parameters in the query.
+		// 1 represents the first parameter in the query.
+		statement.setString(1, dvd.getTitle());
+		statement.setString(2, dvd.getGenre());
+		statement.setInt(3, dvd.getYear());
+		// prints sql statement to console.
 		System.out.println(sql);
-
-		statement.executeUpdate(sql);
+		// executes the sql string that is passed in to the statement
+		// sql does not need to be passed as an argument as the prepared statement holds
+		// it.
+		statement.executeUpdate();
+		// prints "done" to console.
 		System.out.println("done");
-
+		// Closing the connection as if you do not close it it will consume your
+		// resource
+		// continuously and will have overall impact on your application and it
+		// may also not be added or returned to the pool.
 		connection.close();
 
 	}
 
 	public void updateDVD(DVD updateDvd) throws SQLException {
-
+		// creating connection object inside the method
 		Connection connection = getConnection();
-		Statement statement = connection.createStatement();
+		// adding the question marks to protect from SQL injection. question marks
+		// represent parameters that are passed in to the sql
+		// inserts values into title, genre, year field to the ID chosen by the user and
+		// stores it in sql string
+		String sql = "UPDATE collection " + "SET Title = ?, Genre = ?, Year = ?" + " WHERE ID = ?";
+		// PreparedStatement prevents SQL injection and executes faster
+		// statement object created and sql string is passed as an argument.
+		PreparedStatement statement = connection.prepareStatement(sql);
 
-		String sql = "UPDATE collection " + "SET Title = '" + updateDvd.getTitle() + "', Genre = '"
-				+ updateDvd.getGenre() + "', Year = " + updateDvd.getYear() + "" + " WHERE ID = " + updateDvd.getId()
-				+ ";";
+		// the numbers represent the parameters in the query.
+		// 1 represents the first parameter in the query.
+		statement.setString(1, updateDvd.getTitle());
+		statement.setString(2, updateDvd.getGenre());
+		statement.setInt(3, updateDvd.getYear());
+		statement.setInt(4, updateDvd.getId());
+		// prints sql statement to console.
 		System.out.println(sql);
-
-		statement.executeUpdate(sql);
+		// executes the sql string that is passed in to the statement
+		// sql does not need to be passed as an argument as the prepared statement holds
+		// it.
+		statement.executeUpdate();
+		// prints "done" to console.
 		System.out.println("done");
-
+		// Closing the connection as if you do not close it it will consume your
+		// resource
+		// continuously and will have overall impact on your application and it
+		// may also not be added or returned to the pool.
 		connection.close();
 
 	}
 
 	public void deleteDVD(int id) throws SQLException {
-
+		// creating connection object inside the method
 		Connection connection = getConnection();
-		Statement statement = connection.createStatement();
+		// adding the question marks to protect from SQL injection. question marks
+		// represent parameters that are passed in to the sql
+		// deletes title from dvd table (collection) by ID chosen by the user and stores
+		// it in sql string
+		String sql = "DELETE FROM collection " + " WHERE ID = ?";
+		// PreparedStatement prevents SQL injection and executes faster
+		// statement object created and sql string is passed as an argument.
+		PreparedStatement statement = connection.prepareStatement(sql);
 
-		String sql = "DELETE FROM collection " + " WHERE ID = " + id + ";";
-
+		// the numbers represent the parameters in the query.
+		// 1 represents the first parameter in the query.
+		statement.setInt(1, id);
+		// prints sql statement to console.
 		System.out.println(sql);
-
-		statement.executeUpdate(sql);
+		// executes the sql string that is passed in to the statement
+		// sql does not need to be passed as an argument as the prepared statement holds
+		// it.
+		statement.executeUpdate();
+		// prints "done" to console.
 		System.out.println("done");
-
+		// Closing the connection as if you do not close it it will consume your
+		// resource
+		// continuously and will have overall impact on your application and it
+		// may also not be added or returned to the pool.
 		connection.close();
 
 	}
@@ -146,51 +235,84 @@ public class MyDAO {
 
 		// makes connection to the database
 		Connection connection = getConnection();
-		//adding the question marks to protect
-		String sql = "SELECT username, password, apikey,  COUNT (*) as valid FROM Users WHERE username = ? AND password = ?";
-
+		// adding the question marks to protect from SQL injection. question marks
+		// represent parameters that are passed in to the sql
+		// selects the username, password, apikey and counts each row to check if the
+		// username and password matches
+		String sql = "SELECT username, password, apikey, COUNT (*) as valid FROM Users WHERE username = ? AND password = ?";
+		// PreparedStatement prevents SQL injection and executes faster
+		// statement object created and sql string is passed as an argument.
 		PreparedStatement statement = connection.prepareStatement(sql);
-		
-		// add variables to the prepared statement
+
+		// the numbers represent the parameters in the query.
+		// 1 represents the first parameter in the query.
 		statement.setString(1, un);
 		statement.setString(2, pw);
 
+		// prints sql statement to console.
 		System.out.println(sql);
-		ResultSet rs = statement.executeQuery(sql);
+		// executes the sql string that is passed in to the statement
+		// sql does not need to be passed as an argument as the prepared statement holds
+		// it.
+		ResultSet rs = statement.executeQuery();
 
-		// checks the result set
+		// while function iterates over each row until there's no more rows
 		while (rs.next()) {
-			// look in the result set and check if it is valid
+			// look in the result set and check if it is valid.
 			int valid = rs.getInt("valid");
 
 			if (valid > 0) {
+				// prints valid to console if user is valid
 				System.out.println("valid user");
+				// uses getters from Users class and stores them in rs object.
 				String username = rs.getString("username");
 				String password = rs.getString("password");
 				String api = rs.getString("apikey");
-
+				// closes connection if the user is valid
 				connection.close();
 				return new User(username, password, api);
 			}
 
 		}
+		// closes connection if user is not valid.
 		connection.close();
+		// does not return anything if the user is not valid.
 		return null;
 
 	}
 
 	public void insertUser(User user) throws SQLException {
 
+		// makes connection to the database
 		Connection connection = getConnection();
-		Statement statement = connection.createStatement();
 
-		String sql = "INSERT INTO users (username, password, apikey)" + " VALUES ('" + user.getUsername() + "', '"
-				+ user.getPassword() + "', '" + user.getApi() + "');";
+		// adding the question marks to protect from SQL injection. question marks
+		// represent parameters that are passed in to the sql
+		// inserts values into username, password, apikey field and stores it in sql
+		// string
+		String sql = "INSERT INTO users (username, password, apikey)" + " VALUES (?, ?, ?)";
+		// PreparedStatement prevents SQL injection and executes faster
+		// statement object created and sql string is passed as an argument.
+		PreparedStatement statement = connection.prepareStatement(sql);
+
+		// the numbers represent the parameters in the query.
+		// 1 represents the first parameter in the query.
+		// points to user object and uses the getters to retrieve methods
+		statement.setString(1, user.getUsername());
+		statement.setString(2, user.getPassword());
+		statement.setString(3, user.getApi());
+		// prints sql statement to console.
 		System.out.println(sql);
-
-		statement.executeUpdate(sql);
+		// executes the sql string that is passed in to the statement
+		// sql does not need to be passed as an argument as the prepared statement holds
+		// it.
+		statement.executeUpdate();
+		// prints "done" to console.
 		System.out.println("done");
-
+		// Closing the connection as if you do not close it it will consume your
+		// resource
+		// continuously and will have overall impact on your application and it
+		// may also not be added or returned to the pool.
 		connection.close();
 
 	}
@@ -199,26 +321,38 @@ public class MyDAO {
 
 		// makes connection to the database
 		Connection connection = getConnection();
-		Statement statement = connection.createStatement();
+		// counts each row from Users table to check if the apikey matches.
+		String sql = "SELECT COUNT (*) as valid FROM Users WHERE apikey = ?";
+		// PreparedStatement prevents SQL injection and executes faster
+		// statement object created and sql string is passed as an argument.
+		PreparedStatement statement = connection.prepareStatement(sql);
 
-		String sql = "SELECT COUNT (*) as valid FROM Users WHERE apikey = '" + apikey + "';";
-
+		// the numbers represent the parameters in the query.
+		// 1 represents the first parameter in the query.
+		statement.setString(1, apikey);
+		// prints sql statement to console.
 		System.out.println(sql);
-		ResultSet rs = statement.executeQuery(sql);
+		// executes the sql string that is passed in to the statement
+		// sql does not need to be passed as an argument as the prepared statement holds
+		// it.
+		ResultSet rs = statement.executeQuery();
 
 		// checks the result set
 		while (rs.next()) {
 			// look in the result set and check if it is valid
 			int valid = rs.getInt("valid");
+			
 
-			if (valid == 0) {
-				// all good
+			if (valid == 1) {
+				// closes connection if the user is valid
 				connection.close();
 				return true;
 			}
 
 		}
+		// closes connection if user is not valid.
 		connection.close();
+		// returns false if the user is not valid.
 		return false;
 
 	}
